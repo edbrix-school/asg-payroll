bac21ca2bb17dcb8fc681b82a91d9708bfa17927
Sridharan-Official <srajendaran@hexalytics.com>
2026-03-03 12:27:13 +0530

    Intial setup

---
+++ src/main/java/com/asg/payroll/PayrollApplication.java
@@ -0,0 +1,20 @@
+package com.asg.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.asg"})
+@EnableJpaRepositories(basePackages = "com.asg")
+@EntityScan(basePackages = "com.asg")
+public class PayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollApplication.class, args);
+	}
+
+}
