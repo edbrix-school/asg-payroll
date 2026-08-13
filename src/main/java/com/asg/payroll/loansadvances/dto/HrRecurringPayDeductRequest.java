3807f0c415956abe47b0f22da9d9973a550ced68
Kushal Sharma <ksharma@hexalytics.com>
2026-08-13 17:00:34 +0530

    Added validation on the number

--- src/main/java/com/asg/payroll/loansadvances/dto/HrRecurringPayDeductRequest.java
+++ src/main/java/com/asg/payroll/loansadvances/dto/HrRecurringPayDeductRequest.java
@@ -1,5 +1,6 @@
 package com.asg.payroll.loansadvances.dto;
 
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -24,15 +25,18 @@ public class HrRecurringPayDeductRequest {
 
     private String descriptions;
 
+    @Digits(integer = 10, fraction = 3, message = "Total Amount exceeds the maximum allowed limit")
     private BigDecimal totalAmount;
 
     @NotNull(message = "Opening Amount is mandatory")
+    @Digits(integer = 10, fraction = 3, message = "Opening Amount exceeds the maximum allowed limit")
     private BigDecimal openingAmount;
 
     @NotNull(message = "Start Date is mandatory")
     private LocalDate startDate;
 
     @NotNull(message = "Monthly Amount is mandatory")
+    @Digits(integer = 10, fraction = 3, message = "Monthly Amount exceeds the maximum allowed limit")
     private BigDecimal monthlyAmount;
 
     private String settledAndClosed;
